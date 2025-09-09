6ec45b698e299d988b9c1405bfd90fdda9360deb
aletheos <aletheos@outlookcom>
2025-09-09 22:26:40 +0200

    Add partial solution for day 2, task 2 + Input student names and grades ! No grade validation

--- src/dag_2/StudentGrades.java
+++ src/dag_2/StudentGrades.java
@@ -1,7 +1,34 @@
 package dag_2;
 
-public class StudentGrades {
-	public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
+import static java.lang.System.*;
 
+public class StudentGrades {
+	static String[] className = {
+		"Mathematics",
+		"Physics",
+		"English",
+		"Social Studies",
+		"Physical Education"
+	};
+	public static void main(String[] args) {
+		ArrayList<String> students = new ArrayList<String>();
+		Scanner sc = new Scanner(in);
+
+		for (int studentID = 0; ; ++studentID) {
+			out.printf("Enter name of Student #%d, then press <Enter> when done: ", studentID);
+			String studentName = sc.nextLine();
+			if (studentName.isBlank()) { break; }
+			students.addLast(studentName);
+		}
+		if(students.isEmpty()) { return; }
+		int[][] grades = new int[students.size()][5];
+		for (int studentID = 0; studentID < students.size(); ++studentID) {
+			for (int classIndex = 0; classIndex < grades[studentID].length; ++classIndex) {
+				out.printf("What is the grade assigned to %s in %s? ", students.get(studentID), className[classIndex]);
+				grades[studentID][classIndex]=sc.nextInt();
+			}
+		}
 	}
 }
